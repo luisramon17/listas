2e9f39ce19a1fd4da3723cead132085f4ebd00c4
luisramon17 <5749254@clases.edu.sv>
2026-06-05 14:27:04 -0600

    Create funciones.py

---
+++ practica7/funciones.py
@@ -0,0 +1,19 @@
+# ya habiamos visto las predefinidas
+
+def saludar(nombre):
+    print("Hola", nombre)
+
+"""
+saludar("sergio")
+saludar("rodrigo")
+saludar("juan")
+"""
+
+# celsius a farenhait: (c * 1.8) + 32
+
+def convertir_a_farenheit(c):
+    return (c * 1.8) + 32
+
+print(convertir_a_farenheit(10))
+print(convertir_a_farenheit(30))
+print(convertir_a_farenheit(80))
